b918c07e8031925439165b40247d5f88796cd705
Simon <simon@opalsolutions.be>
2020-03-10 00:04:43 +0100

    first commit

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Conference;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create('fr_FR');
+
+       for($i =0; $i<10 ; $i++){
+           $conf = new Conference();
+           $conf->setName($faker->city);
+           $conf->setDescription($faker->text);
+           $conf->setHourStart(new \DateTime());
+           $manager->persist($conf);
+       }
+
+        $manager->flush();
+    }
+}
